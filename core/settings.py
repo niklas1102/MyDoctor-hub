ffff7d15813d6648a6582a31e0f5107a3011b94e
Niklas <niklas.inderst@gmail.com>
2024-10-21 15:21:24 +0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,13 @@ DEBUG = str2bool(os.environ.get('DEBUG'))
 #print(' DEBUG -> ' + str(DEBUG) ) 
 
 # Hosts Settings
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://mydoctor-hub-c052677a5661.herokuapp.com/']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    'mydoctor-hub.herokuapp.com', 
+    os.getenv('HEROKU_HOST', 'mydoctor-hub-c052677a5661.herokuapp.com')
+]
+
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'https://rocket-django.onrender.com']
 
 # Used by DEBUG-Toolbar 
